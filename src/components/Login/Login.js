854e404c6fc1a7b2fb7b8d1cfe7db7298eb87244
Nikhil <nikhil@nikhils-mbp.myfiosgateway.com>
2021-01-14 02:02:44 -0500

    Adding login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,15 +1,51 @@
-import {Component} from "react";
+import React, {Component} from "react";
+import {Form} from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 
 class Login extends Component{
     constructor(props) {
         super(props);
-        this.state = {};
+        // const [email, setEmail] = useState("");
+        // const [password, setPassword] = useState("");
+        this.state = {
+            email: "",
+            password: ""
+        };
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    validateForm = () => {
+        return this.state.email.length > 0 && this.state.password.length > 0;
     }
 
     render() {
         return (
-            <div>
-                This is Login Page
+            <div className="Login">
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Group size="lg" controlId="email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control
+                            autoFocus
+                            type="email"
+                            value={this.state.email}
+                            onChange={(e) => this.setState({email:e.target.value})}
+                        />
+                    </Form.Group>
+                    <Form.Group size="lg" controlId="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            type="password"
+                            value={this.state.password}
+                            onChange={(e) => this.setState({password:e.target.value})}
+                        />
+                    </Form.Group>
+                    <Button block size="lg" type="submit" disabled={!this.validateForm()}>
+                        Login
+                    </Button>
+                </Form>
             </div>
         );
     }
