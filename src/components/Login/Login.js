02e73c4996140697714d7b45559cca585d8a6800
Nikhil <nikhil@nikhils-mbp.myfiosgateway.com>
2021-02-19 01:42:12 -0500

    adding login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,19 +1,44 @@
 import React, {Component} from "react";
 import {Form} from "react-bootstrap";
 import Button from "react-bootstrap/Button";
+import axios from "axios";
 
-class Login extends Component{
+class Login extends Component {
     constructor(props) {
         super(props);
         // const [email, setEmail] = useState("");
         // const [password, setPassword] = useState("");
         this.state = {
             email: "",
-            password: ""
+            password: "",
+            isValidUser: false
         };
     }
 
-    handleSubmit = (event) => {
+    handleSubmit = async (event) => {
+        console.log(event)
+        let config = {
+            headers: {
+                "content-type": "application/json",
+            }
+        };
+        let data = {
+            "email": event.target[0].defaultValue,
+            "password": event.target[1].defaultValue
+        };
+        let isValidUser = await axios.post("http://localhost:8081/login/",data,config);
+        alert(JSON.stringify(isValidUser));
+
+        if(isValidUser["data"] == "true") {
+            this.setState({
+                email: event.target[0].defaultValue,
+                password: event.target[1].defaultValue,
+                isValidUser: true
+            });
+        }
+
+        console.log(JSON.stringify(isValidUser));
+
         event.preventDefault();
     }
 
@@ -31,7 +56,7 @@ class Login extends Component{
                             autoFocus
                             type="email"
                             value={this.state.email}
-                            onChange={(e) => this.setState({email:e.target.value})}
+                            onChange={(e) => this.setState({email: e.target.value})}
                         />
                     </Form.Group>
                     <Form.Group size="lg" controlId="password">
@@ -39,10 +64,10 @@ class Login extends Component{
                         <Form.Control
                             type="password"
                             value={this.state.password}
-                            onChange={(e) => this.setState({password:e.target.value})}
+                            onChange={(e) => this.setState({password: e.target.value})}
                         />
                     </Form.Group>
-                    <Button block size="lg" type="submit" disabled={!this.validateForm()}>
+                    <Button block size="lg" type="submit">
                         Login
                     </Button>
                 </Form>
